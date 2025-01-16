cbfdf4449f3f5db48426bbf783aba09c0a358ac5
maulaputra <maulanaputra2003@gmail.com>
2025-01-16 18:53:07 +0700

    first commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Blog</title>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
+    <h1>Halaman Blog</h1>
+
+    <article>
+        <h3>Latihan Laravel 11</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus dignissimos ullam reprehenderit impedit officiis, molestiae maiores exercitationem quis eligendi aut explicabo quos facilis consectetur. Repellendus reiciendis ipsa optio aperiam delectus?</p>
+    </article>
+
+    <article>
+        <h3>Latihan Dasar Flutter</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit hic nobis est quaerat accusamus modi error dolores at dignissimos, a eveniet repudiandae fugiat inventore, eos voluptas, aspernatur voluptatibus magni ipsa?</p>
+    </article>
+</body>
+</html>
