e663360cdfdf2b66cf1828b9bc615c4af94d8c28
maulaputra <maulanaputra2003@gmail.com>
2025-01-17 21:10:49 +0700

    hari ke 3

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,19 @@ Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
 
-Route::get('/about', function () {
-    return view('about', ['title' => 'About Page']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'About Page', 'posts' => [
+        [
+            'title' => 'Judul Artikel 1',
+            'author' => 'Maulana Putra',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate molestias delectus blanditiis minima reprehenderit reiciendis adipisci repellat quibusdam aliquid perferendis, sed asperiores tempora culpa atque aspernatur nisi ut quae suscipit?'
+        ],
+        [
+            'title' => 'Judul Artikel 2',
+            'author' => 'Maulana Putra',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quia dignissimos veritatis iusto quaerat quas autem eius obcaecati laboriosam, voluptates quo eaque saepe fugiat unde culpa natus odio voluptatem accusamus.'
+        ]
+    ]]);
 });
 
 Route::get('/blog', function () {
