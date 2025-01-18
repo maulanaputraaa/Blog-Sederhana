34d30bb1e9e97bde8960d72c409014a919bbb074
maulaputra <maulanaputra2003@gmail.com>
2025-01-18 11:19:57 +0700

    lanjutan metode view data kemarin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,11 +10,15 @@ Route::get('/', function () {
 Route::get('/posts', function () {
     return view('posts', ['title' => 'About Page', 'posts' => [
         [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
             'title' => 'Judul Artikel 1',
             'author' => 'Maulana Putra',
             'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate molestias delectus blanditiis minima reprehenderit reiciendis adipisci repellat quibusdam aliquid perferendis, sed asperiores tempora culpa atque aspernatur nisi ut quae suscipit?'
         ],
         [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
             'title' => 'Judul Artikel 2',
             'author' => 'Maulana Putra',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quia dignissimos veritatis iusto quaerat quas autem eius obcaecati laboriosam, voluptates quo eaque saepe fugiat unde culpa natus odio voluptatem accusamus.'
@@ -21,8 +26,34 @@ Route::get('/posts', function () {
     ]]);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog Page']);
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+            [
+                'id' => 1,
+                'slug' => 'judul-artikel-1',
+                'title' => 'Judul Artikel 1',
+                'author' => 'Maulana Putra',
+                'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate molestias delectus blanditiis minima reprehenderit reiciendis adipisci repellat quibusdam aliquid perferendis, sed asperiores tempora culpa atque aspernatur nisi ut quae suscipit?'
+            ],
+            [
+                'id' => 2,
+                'slug' => 'judul-artikel-2',
+                'title' => 'Judul Artikel 2',
+                'author' => 'Maulana Putra',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quia dignissimos veritatis iusto quaerat quas autem eius obcaecati laboriosam, voluptates quo eaque saepe fugiat unde culpa natus odio voluptatem accusamus.'
+            ]
+        ];
+
+        $post = Arr::first($posts, function($post) use ($slug) {
+            return $post['slug'] == $slug;
+        });
+
+        return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+
+Route::get('/about', function () {
+    return view('about', ['title' => 'About Page']);
 });
 
 Route::get('/contact', function () {
